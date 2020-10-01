61063b828302fea3351f554d915f752547ef9f7a
s_sangole <s_sangole@apple.com>
2020-10-01 15:12:54 -0700

    Learn docker-kub-aws

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React or Angular you decide Okay changed
         </a>
       </header>
     </div>
