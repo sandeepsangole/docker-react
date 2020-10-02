e5caa4c7c5a79429019192357e13093d8c97bcd0
s_sangole <s_sangole@apple.com>
2020-10-02 14:43:29 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Docker AWS Learning !!!
+          Docker AWS Learning
         </a>
       </header>
     </div>
